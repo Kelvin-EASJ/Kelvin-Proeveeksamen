2b1c1851acdecf4a8391bf5135693191b572cb68
Kelvin-EASJ <kelvin.aggerholm@gmail.com>
2020-12-16 13:32:54 +0100

    Forsøg på HTTP-Server

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,17 @@
+const { response } = require('express');
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path'); 
+
+fetch('https://vejr.eu/api.php?location=N%C3%A6stved&degree=C')
+.then(response => {
+  return response.json();
+})
+.then(users => {
+  console.log(users)
+})
+app.use('/static', express.static(path.join(__dirname, 'Sensor')));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
